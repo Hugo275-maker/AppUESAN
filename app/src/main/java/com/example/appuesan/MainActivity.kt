7dfadad1a279b9802191b7f5b71d50ba3e833390
Hugo Flores <24200293@ue.edu.pe>
2026-09-12 15:59:14 -0500

    Cajas de Texto y boton Iniciar Sesion

--- app/src/main/java/com/example/appuesan/MainActivity.kt
+++ app/src/main/java/com/example/appuesan/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.appuesan.presentation.auth.LoginScreen
 import com.example.appuesan.ui.theme.AppUESANTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppUESANTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppUESANTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/appuesan/presentation/auth/RegisterScreen.kt
@@ -0,0 +1,4 @@
+package com.example.appuesan.presentation.auth
+
+class RegisterScreen {
+}
